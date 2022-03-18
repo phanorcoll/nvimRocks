a28a73f5109611f953bca34b5571c28ccc6456f5
Phanor Coll <phanorcoll@gmail.com>
2022-03-18 17:34:13 -0400

    Updated nightfox theme configuration

--- lua/mrdupin/colorscheme.lua
+++ lua/mrdupin/colorscheme.lua
@@ -7,15 +7,17 @@ if not status_ok then
 end
 
 nightfox. setup({
-	fox = "nordfox", -- change the colorscheme to use nordfox
-  transparent = true, -- Disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening :terminal
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-    keywords = "bold", -- change style of keywords to be bold
-    functions = "italic,bold" -- styles can be a comma separated list
+  options = {
+    terminal_colors = true, -- Configure the colors used when opening :terminal
+    transparent = true, -- Disable setting the background color
+    styles = {
+      comments = "italic", -- change style of comments to be italic
+      keywords = "bold", -- change style of keywords to be bold
+      functions = "italic,bold" -- styles can be a comma separated list
+    },
   },
-  colors = {
+  dim_inactive = false,   -- Non focused panes set to alternative background
+  pallets = {
     red = "#FF000", -- Override the red color for MAX POWER
     bg_alt = "#000000",
   },
@@ -25,4 +27,4 @@ nightfox. setup({
   }
 })
 
-nightfox. load()
+vim.cmd("colorscheme nightfox")
