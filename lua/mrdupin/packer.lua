360632a03b9d50595f2566d645eb27b797ebb98b
Phanor Coll <phanorcoll@gmail.com>
2022-11-05 19:08:31 -0400

    🎨 installed(lualine)
    installed package lualine and created config file.

--- lua/mrdupin/packer.lua
+++ lua/mrdupin/packer.lua
@@ -51,6 +51,11 @@ return packer.startup(function(use)
   tag = 'nightly' -- optional, updated every week. (see issue #1193)
 }
 
+use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	}) -- statusline written in Lua.
+
 -- Theme
 	use("EdenEast/nightfox.nvim")
 
