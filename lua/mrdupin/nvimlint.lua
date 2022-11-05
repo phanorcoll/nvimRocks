7a9ac177db1113d1c01b6450a03ba9d870eacc39
Phanor Coll <phanorcoll@gmail.com>
2022-11-04 20:40:16 -0400

    🔥 refactor
    deleted old files

--- lua/mrdupin/nvimlint.lua
+++
@@ -1,9 +0,0 @@
-local lint_status_ok, lint = pcall(require, "lint")
-if not lint_status_ok then
-	vim.notify("lint not found!!", "error")
-	return
-end
-
-lint.linters_by_ft = {
-	markdown = { "eslint", "markdownlint" },
-}
