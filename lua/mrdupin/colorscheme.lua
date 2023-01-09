cf569f2981591c6120b1eedacab87c7d04294a3e
Phanor Coll <phanorcoll@gmail.com>
2023-01-09 17:21:03 -0400

    🐛 Bug with Theme
    Fixed issue with background on theme, transparency wasnt being applied.
    
    ✏️ related to #2

---
+++ lua/mrdupin/colorscheme.lua
@@ -0,0 +1,11 @@
+require('rose-pine').setup({
+	--- @usage 'main' | 'moon'
+	dark_variant = 'moon',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = true,
+	disable_float_background = true,
+	disable_italics = false,
+})
+
+vim.cmd('colorscheme rose-pine')
--- lua/mrdupin/init.lua
+++ lua/mrdupin/init.lua
@@ -1,3 +1,4 @@
 require("mrdupin.packer")
 require("mrdupin.remap")
+require("mrdupin.colorscheme")
 print("hello from mrdupin")
