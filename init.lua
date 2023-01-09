f8f8d9ddf30c25740402edb27803d7a470ac5d8f
Phanor Coll <phanorcoll@gmail.com>
2023-01-09 09:22:00 -0400

    ⚰️ Initialized configuration.
    Starting from scratch to make deeper modification without
    having dead code or config collision.
    
    ✏️ no task related.

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
---TODO: set for typscript development, format, tsx, jsx, js, ts files, autocomplete, lsp, format on save.
 --TODO: set for HTML, CSS development, format css, html files, autocomplete, lsp, format on save.
 --TODO: set key mappings
 --TODO: set productivity plugins
--- lua/mrdupin/lualine.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, lualine = pcall(require, "lualine")
-if not status_ok then
-	vim.notify("lualine not found", "error")
-	return
-end
-
-lualine.setup{
-options = { theme  = "dracula" },
-}
