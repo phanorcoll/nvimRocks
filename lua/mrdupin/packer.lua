f47eb8c09f96437459c0e0495874255f3ee3cb7e
Phanor Coll <phanorcoll@gmail.com>
2023-01-10 09:52:29 -0400

    🎨 Lualine
    Configured lualine package
    
    ✏️ close #18

--- lua/mrdupin/packer.lua
+++ lua/mrdupin/packer.lua
@@ -106,4 +106,10 @@ return packer.startup(function(use)
         requires = "nvim-lua/plenary.nvim"
     }
 
+    -- lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
 end)
