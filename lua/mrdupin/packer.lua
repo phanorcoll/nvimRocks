3d616d61108d5b649b19aea33958ad003395c2de
Phanor Coll <phanorcoll@gmail.com>
2023-01-10 10:35:35 -0400

    🚨 Diagnostics package
    setup Trouble packafe for showing diagnostics, references,
    telescope results, quickfix and location lists.
    
    ✏️ close #11

--- lua/mrdupin/packer.lua
+++ lua/mrdupin/packer.lua
@@ -115,4 +115,20 @@ return packer.startup(function(use)
     -- An asynchronous linter plugin
     use({'mfussenegger/nvim-lint'})
 
+    -- web icons
+    use ('nvim-tree/nvim-web-devicons')
+
+    -- A pretty list for showing diagnostics, references, telescope results, quickfix and location
+    use {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
 end)
