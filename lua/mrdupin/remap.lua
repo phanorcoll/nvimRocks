256105eb41eadcf55d0557212ece4852fcb7c359
Phanor Coll <phanorcoll@gmail.com>
2023-01-10 20:51:32 -0400

    🎨 New remaps
    Added split navigation and resizing.
    
    ✏️ close #19

--- lua/mrdupin/remap.lua
+++ lua/mrdupin/remap.lua
@@ -27,6 +27,20 @@ vim.keymap.set("n","<leadeer>f", function() vim.lsp.bug.format() end)
 -- makes a bash file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", opts)
 
+-- Better window navigation
+vim.keymap.set("n", "<S-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<S-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<S-l>", "<C-w>l", opts)
+
+vim.keymap.set("n","<S-x>", ":bd<cr>", opts)
+
+-- Resize with arrows
+vim.keymap.set("n", "<S-Up>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<S-Down>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<S-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<S-Right>", ":vertical resize +2<CR>", opts)
+
 
 -- Breaking a bad habit
 vim.keymap.set("n", "<Up>", [[:echoerr "Dont use arrow keys!!"<cr>]], { noremap = true })
