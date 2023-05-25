8cb3b32bd81a2c4cad8dc6ae6981064c34079682
Phanor Coll <phanorcoll@gmai.com>
2023-05-25 12:49:02 -0400

    chore: configure cmp
    also fixed issue with packer, added script to install it if it doesnt
    exists.

--- lua/mrdupin/plugins.lua
+++ lua/mrdupin/plugins.lua
@@ -1,3 +1,28 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 -- Automatically run: PackerCompile
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
@@ -62,6 +87,21 @@ return require("packer").startup(function(use)
       -- follow latest release.
       tag = "v<CurrentMajor>.*",
   })
+
+  -- cmp: Autocomplete
+  use({
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      config = function()
+          require("mrdupin.configs.cmp")
+      end,
+  })
+
+  use("hrsh7th/cmp-nvim-lsp")
+
+  use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
+
+  use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
 
   
 end)
