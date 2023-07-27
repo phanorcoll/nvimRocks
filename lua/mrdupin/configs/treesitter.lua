2daf5f961607f674528f8c141862d30042cd89b4
Phanor Coll <phanorcoll@gmail.com>
2023-07-27 10:30:36 -0400

    format code

--- lua/mrdupin/configs/treesitter.lua
+++ lua/mrdupin/configs/treesitter.lua
@@ -1,49 +1,45 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if not status then
-    return
+  return
 end
 
 ts.setup({
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
-    ensure_installed = {
-        "markdown",
-        "tsx",
-        "typescript",
-        "javascript",
-        "toml",
-        "json",
-        "yaml",
-        "rust",
-        "css",
-        "html",
-        "lua",
-        "go",
-        "gomod",
-        "gowork",
-        "gitcommit",
-        "gitignore",
-        "terraform",
-        "bash",
-        "dockerfile",
-        "svelte",
-        "markdown",
-    },
-    rainbow = {
-        enable = true,
-        disable = { "html" },
-        extended_mode = false,
-        max_file_lines = nil,
-    },
-    autotag = { enable = true },
-    incremental_selection = { enable = true },
-    indent = { enable = true },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  ensure_installed = {
+    "markdown",
+    "tsx",
+    "typescript",
+    "javascript",
+    "toml",
+    "json",
+    "yaml",
+    "rust",
+    "css",
+    "html",
+    "lua",
+    "go",
+    "gomod",
+    "gowork",
+    "gitcommit",
+    "gitignore",
+    "terraform",
+    "bash",
+    "dockerfile",
+    "svelte",
+    "markdown",
+  },
+  rainbow = {
+    enable = true,
+    disable = { "html" },
+    extended_mode = false,
+    max_file_lines = nil,
+  },
+  autotag = { enable = true },
+  incremental_selection = { enable = true },
+  indent = { enable = true },
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
