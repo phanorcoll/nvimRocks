e4915cc7e4057b4e8116e579b0b97c314ab048d2
Phanor Coll <phanorcoll@gmai.com>
2023-08-20 09:08:38 -0400

    added auto format and imports when save on Go files

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -50,6 +50,19 @@ return {
           return
         end
 
+        if client.name == 'gopls' then
+          -- Run gofmt + goimport on save
+          local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+              require('go.format').goimport()
+            end,
+            group = format_sync_grp,
+          })
+          return
+        end
+
         -- Create an autocmd that will run *before* we save the buffer.
         --  Run the formatting command for the LSP that has just attached.
         vim.api.nvim_create_autocmd('BufWritePre', {
