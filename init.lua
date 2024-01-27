73bdabba68c4a418c0fe0548e423c3dadfb2c939
Phanor Coll <phanorcoll@gmail.com>
2024-01-27 12:03:42 -0400

    updated indent-blankline configuration

--- init.lua
+++ init.lua
@@ -162,12 +162,10 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
